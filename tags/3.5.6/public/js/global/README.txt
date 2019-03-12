361b58c6d334fffa1abeac7cca277fe989bb6026
Aquafortis <support@320up.com>
2019-03-12 20:37:48 +1000

    Tagged version 3.5.6

---
+++ tags/3.5.6/public/js/global/README.txt
@@ -0,0 +1,17 @@
+
+This folder contains a Global script for the Plugin.
+
+If you are having trouble with the standard script, replace it with this one.
+
+Copy this script into /wp-content/plugins/woo-align-buttons/public/js/wooalign-public.js
+to replace the original script.
+
+What's the difference?
+
+This global script has more options to trigger the alignment of the buttons.
+
+It will also trigger on "scroll" and "on mouse move".
+
+The scroll function will help with "Ajax Load More" scripts.
+
+This global script can also be called from other Plugin's by using: wooAlignButtons();
---
+++ tags/3.5.6/public/js/wooalign-public.js
@@ -0,0 +1,63 @@
+/*! @preserve
+ * Plugin Name:       Woo Align Buttons
+ * Plugin URI:        https://wordpress.org/plugins/woo-align-buttons
+ * Description:       A lightweight plugin to align WooCommerce "Add to cart" buttons.
+ * Version:           3.5.6
+ * Author:            320up
+ * Author URI:        https://320up.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+(function($) {
+  "use strict";
+  if ($("ul.products").length) {
+    var wooAlignButtons = function() {
+      $("ul.products").each(function() {
+        var gridRows = [];
+        var tempRow = [];
+        var wooGridElements = $(this).children("li");
+        wooGridElements.each(function(index) {
+          if ($(this).css("clear") !== "none" && index !== 0) {
+            gridRows.push(tempRow);
+            tempRow = [];
+          }
+          tempRow.push(this);
+          if (wooGridElements.length === index + 1) {
+            gridRows.push(tempRow);
+          }
+        });
+        var wooheight = "#woo-height";
+        var tallestWoo = 0;
+        $(this).each(function() {
+          $(this).find(wooheight).css({
+            "min-height": "",
+            "padding-bottom": ""
+          });
+          var wooHeightInfo = $(this).find(wooheight).height();
+          var wooSpacing = 10;
+          var totalHeight = wooHeightInfo + wooSpacing;
+          if (totalHeight > tallestWoo) {
+            tallestWoo = totalHeight;
+          }
+        });
+        $(this).each(function() {
+          $(this).find(wooheight).css("min-height", tallestWoo);
+        });
+      });
+    };
+    wooAlignButtons();
+    $(window).on("load", function() {
+      wooAlignButtons();
+      $(window).on("hold-your-horses", function() {
+        wooAlignButtons();
+      });
+    });
+  }
+  var resizeTimer;
+  $(window).resize(function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function() {
+      $(window).trigger("hold-your-horses");
+    }, 250);
+  });
+})(jQuery);
